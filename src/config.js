4299ee5cc56ea7e369aae0bf23ecf3d4cd1a5338
Wany <sung@wanyne.com>
2024-09-12 12:42:51 +0900

    up

--- src/config.js
+++ src/config.js
@@ -6,31 +6,33 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dir = path.resolve(__dirname, '../config');
-
 let config = {};
 
-for (const filename of fs.readdirSync(dir)) {
-  if (path.extname(filename) != '.json') {
-    continue;
-  }
-  const filepath = dir + '/' + filename;
-  if (fs.lstatSync(filepath).isDirectory()) {
-    continue;
-  }
-  let json = {};
-  let key = path.basename(filename, '.json');
-  try {
-    json = JSON.parse(fs.readFileSync(filepath));
-  } catch (error) {
-    throw error;
-  }
-  if (key == 'config') {
-    for (const k in json) {
-      config[k] = json[k];
+const dir = path.resolve(__dirname, '../config');
+
+if (fs.existsSync(dir)) {
+  for (const filename of fs.readdirSync(dir)) {
+    if (path.extname(filename) != '.json') {
+      continue;
     }
-  } else {
-    config[key] = json;
+    const filepath = dir + '/' + filename;
+    if (fs.lstatSync(filepath).isDirectory()) {
+      continue;
+    }
+    let json = {};
+    let key = path.basename(filename, '.json');
+    try {
+      json = JSON.parse(fs.readFileSync(filepath));
+    } catch (error) {
+      throw error;
+    }
+    if (key == 'config') {
+      for (const k in json) {
+        config[k] = json[k];
+      }
+    } else {
+      config[key] = json;
+    }
   }
 }
 
